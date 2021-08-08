f86208fde4bcb56a551525e4f575a2c97a4d86ec
enjay27 <kdkm2002@gmail.com>
2021-08-08 21:08:26 +0900

    백준 2941 2999

--- src/baekjoon/Solution2941.java
+++ src/baekjoon/Solution2941.java
@@ -1,4 +1,43 @@
 package baekjoon;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Solution2941 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<String> alphabet = new ArrayList<>();
+        alphabet.add("c=");
+        alphabet.add("c-");
+        alphabet.add("dz=");
+        alphabet.add("d-");
+        alphabet.add("lj");
+        alphabet.add("nj");
+        alphabet.add("s=");
+        alphabet.add("z=");
+
+        String input = sc.next();
+
+        char[] chars = input.toCharArray();
+
+        StringBuffer sb = new StringBuffer();
+        int count = 0;
+        for (char aChar : chars) {
+            sb.append(aChar);
+            for (String s : alphabet) {
+                if (sb.toString().contains(s)) {
+                    count += sb.length() - s.length() + 1;
+                    sb.setLength(0);
+                    break;
+                }
+            }
+        }
+
+        count += sb.length();
+
+        System.out.println("count = " + count);
+
+    }
 }
--- src/baekjoon/Solution2999.java
+++ src/baekjoon/Solution2999.java
@@ -1,4 +1,46 @@
 package baekjoon;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
 public class Solution2999 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+
+        List<Integer> divisor = new ArrayList<>();
+        for (int i = 2; i <= Math.sqrt(input.length()); i++) {
+            if (input.length() % i == 0) {
+                divisor.add(i);
+            }
+        }
+
+        int colSize;
+        if (divisor.isEmpty()) {
+            colSize = input.length();
+        } else {
+            colSize = divisor.stream().reduce(0, (a, b) -> a < b ? b : a);
+        }
+
+        int rowSize = input.length() / colSize;
+
+        char[][] inputArr = new char[rowSize][colSize];
+
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < colSize; j++) {
+                inputArr[i][j] = input.charAt(i * colSize + j);
+            }
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < colSize; i++) {
+            for (int j = 0; j < rowSize; j++) {
+                bw.write(inputArr[j][i]);
+            }
+        }
+        bw.flush();
+    }
 }

f4866c3f9ec7b6f528a1631222ba419b3474fb7e
enjay27 <kdkm2002@gmail.com>
2021-08-24 13:22:56 +0900

    감소숫자 완료

--- src/baekjoon/Solution1038.java
+++ src/baekjoon/Solution1038.java
@@ -11,17 +11,20 @@ public class Solution1038 {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
 
-//        backtracking(number, 0, 0);
+        //1021 : 987654321
+        //1022 : 9876543210
         int next = 0;
-        if (number > 1038) {
-            next = -1;
+        if (number == 1022) {
+            System.out.println("9876543210");
+        } else if (number > 1022) {
+            System.out.println(-1);
         } else {
             for (int i = 0; i < number; i++) {
-                next = nextDecreasingNumber(next + 1, Integer.toString(next).length() - 1);
+                next = nextDecreasingNumberV2(next + 1);
             }
+            System.out.println(next);
         }
 
-        System.out.println(next);
     }
 
     static void backtracking(int searchCount, int curCount, Integer curNumber) {
@@ -59,6 +62,34 @@ public class Solution1038 {
         }
 
         return next;
+    }
+
+    static int backtrackingV2(Integer curNum, int curIndex) {
+        String s = curNum.toString();
+        if (curIndex == 0) return -1;
+
+        if (curIndex <= s.length() - 1) {
+            if (s.charAt(curIndex) >= s.charAt(curIndex - 1)) {
+                return curIndex - 1;
+            } else {
+                return backtrackingV2(curNum, curIndex - 1);
+            }
+        }
+
+        return -1;
+    }
+
+    static int nextDecreasingNumberV2(Integer curNum) {
+        String s = curNum.toString();
+        int index = backtrackingV2(curNum, s.length() - 1);
+        while (index != -1) {
+            int pow = (int) Math.pow(10, s.length() - index - 1);
+            curNum = curNum / pow * pow;
+            curNum += pow;
+            s = curNum.toString();
+            index = backtrackingV2(curNum, s.length() - 1);
+        }
+        return curNum;
     }
 
 }

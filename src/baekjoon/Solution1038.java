706166d6a8683670cbf212e072e9e9c8705140e7
enjay27 <kdkm2002@gmail.com>
2021-08-24 14:29:12 +0900

    감소숫자 완료

--- src/baekjoon/Solution1038.java
+++ src/baekjoon/Solution1038.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
 
 public class Solution1038 {
 
-    static boolean isEnd = false;
-    static int result = -1;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
@@ -20,74 +17,40 @@ public class Solution1038 {
             System.out.println(-1);
         } else {
             for (int i = 0; i < number; i++) {
-                next = nextDecreasingNumberV2(next + 1);
+                next = nextDecreasingNumber(next + 1);
             }
             System.out.println(next);
         }
 
     }
 
-    static void backtracking(int searchCount, int curCount, Integer curNumber) {
-        if (isEnd) return;
-
-        if (curCount == searchCount) {
-            isEnd = true;
-            result = curNumber;
-            return;
-        }
-
-        if (curNumber > 1000000) {
-            isEnd = true;
-            return;
-        }
-
-        int nextNumber = nextDecreasingNumber(curNumber + 1, curNumber.toString().length() - 1);
-        backtracking(searchCount, curCount + 1, nextNumber);
-    }
-
-    static int nextDecreasingNumber(Integer curNum, int curIndex) {
-        String s = curNum.toString();
-        if (curIndex == 0) return curNum;
-
-        int next = -1;
-        if (curIndex <= s.length() - 1) {
-            if (s.charAt(curIndex) < s.charAt(curIndex - 1)) {
-                next = nextDecreasingNumber(curNum, curIndex - 1);
-            } else {
-                int pow = (int) Math.pow(10, s.length() - curIndex);
-                curNum = curNum / pow * pow;
-                curNum += pow;
-                next = nextDecreasingNumber(curNum, curNum.toString().length() - 1);
-            }
-        }
-
-        return next;
-    }
-
-    static int backtrackingV2(Integer curNum, int curIndex) {
-        String s = curNum.toString();
+    static int backtracking(Integer curNum, int curIndex) {
         if (curIndex == 0) return -1;
+
+        String s = curNum.toString();
 
         if (curIndex <= s.length() - 1) {
             if (s.charAt(curIndex) >= s.charAt(curIndex - 1)) {
                 return curIndex - 1;
             } else {
-                return backtrackingV2(curNum, curIndex - 1);
+                return backtracking(curNum, curIndex - 1);
             }
         }
 
         return -1;
     }
 
-    static int nextDecreasingNumberV2(Integer curNum) {
+    static int nextDecreasingNumber(Integer curNum) {
         String s = curNum.toString();
-        int index = backtrackingV2(curNum, s.length() - 1);
+
+        int index = backtracking(curNum, s.length() - 1);
+
         while (index != -1) {
             int pow = (int) Math.pow(10, s.length() - index - 1);
             curNum = curNum / pow * pow;
             curNum += pow;
             s = curNum.toString();
-            index = backtrackingV2(curNum, s.length() - 1);
+            index = backtracking(curNum, s.length() - 1);
         }
         return curNum;
     }
